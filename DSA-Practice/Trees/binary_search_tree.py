046fe142f32eff7dbaa6bd5e48cd037bbf8214ce
K Shamanth Kumar <kshamanth97@gmail.com>
2025-07-10 21:36:38 +0200

    addition of BST

--- DSA-Practice/Trees/binary_search_tree.py
+++ DSA-Practice/Trees/binary_search_tree.py
@@ -1,40 +1,41 @@
 class Node:
+    """Node class for the Binary Search Tree class"""
     def __init__(self, value):
         self.value = value
-        self.right = None
         self.left = None
-
-
+        self.right = None
+    
 class BinarySearchTree:
     def __init__(self):
         self.root = None
-
+    
     def insert(self, value):
+        """Method to insert a node into BST"""
         new_node = Node(value)
 
-        if self.root is None:
+        if self.root == None:
             self.root = new_node
-            return True
         
         temp = self.root
+
         while True:
-            if temp.value == new_node.value:
+            if new_node.value == temp.value:
                 return False
-            
             if new_node.value < temp.value:
                 if temp.left is None:
                     temp.left = new_node
+                    return True
                 temp = temp.left
             else:
                 if temp.right is None:
                     temp.right = new_node
+                    return True
                 temp = temp.right
     
     def contains(self, value):
-        if self.root is None:
-            return False
-        
+        """Method to check the availability of the node"""
         temp = self.root
+
         while temp is not None:
             if value < temp.value:
                 temp = temp.left
@@ -42,19 +43,20 @@ class BinarySearchTree:
                 temp = temp.right
             else:
                 return True
+        return False
 
-        return False 
 
 if __name__ == '__main__':
     bst = BinarySearchTree()
     bst.insert(2)
-    bst.insert(1)
     bst.insert(3)
-    bst.insert(2)
-    bst.insert(5)
-
-    print(bst.contains(5))
+    bst.insert(1)
+    bst.insert(4)
 
     print(bst.root.value)
     print(bst.root.left.value)
     print(bst.root.right.value)
+    print(bst.root.right.right.value)
+
+    print(bst.contains(2))
+
